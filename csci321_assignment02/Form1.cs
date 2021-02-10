14dd260d85526615ce4267f4e05445d3acfe89ad
CDAGaming <cstack2011@yahoo.com>
2021-02-10 17:43:57 -0600

    Removed deprecated TestData

--- csci321_assignment02/Form1.cs
+++ csci321_assignment02/Form1.cs
@@ -356,45 +356,5 @@ namespace csci321_assignment02
             return parsedData;
         }
 
-        public void ConsoleTest()
-        {
-            try
-            {
-                var board = new Board();
-                Console.WriteLine("Please enter 'N M W' params");
-                board.InitializeBasicParams();
-
-                for (int i = 0; !(board.Size == 0 && board.MarblesCount == 0 && board.WallsCount == 0); i++)
-                {
-                    //Initialize marbles
-                    Console.WriteLine("Please enter marble positions");
-                    board.InitializeBoardObjects(BoardObjectTypes.Marble);
-
-                    //Initialize holes
-                    Console.WriteLine("Please enter hole positions");
-                    board.InitializeBoardObjects(BoardObjectTypes.Hole);
-
-                    //Initialize walls
-                    Console.WriteLine("Please enter wall positions");
-                    board.InitializeBoardObjects();
-
-                    //check for possible solution
-                    var solution = board.FindRoute(string.Empty);
-                    Console.WriteLine("Case {0}: {1}\n", i + 1, solution.Success ? solution.Route.Length + " moves " + solution.Route : "impossible");
-
-                    //try again
-                    Console.WriteLine("Please enter 'N M W' params");
-                    board = new Board();
-                    board.InitializeBasicParams();
-                }
-
-                Console.WriteLine("You finished the test!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
     }
 }
