54bf09e921c0ecf749956c0d66532a1d7d34eee8
CDAGaming <cstack2011@yahoo.com>
2021-02-15 18:49:32 -0600

    Sync Data

--- csci321_assignment02/MarbleExplorer.Designer.cs
+++ csci321_assignment02/MarbleExplorer.Designer.cs
@@ -29,13 +29,21 @@ namespace csci321_assignment02
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MarbleExplorer));
             this.BackButton = new System.Windows.Forms.Button();
             this.ForwardButton = new System.Windows.Forms.Button();
             this.OpenButton = new System.Windows.Forms.Button();
             this.PathLabel = new System.Windows.Forms.Label();
             this.PathText = new System.Windows.Forms.TextBox();
-            this.DataBrowser = new System.Windows.Forms.WebBrowser();
             this.ExploreButton = new System.Windows.Forms.Button();
+            this.FilePreviewBox = new System.Windows.Forms.PictureBox();
+            this.DataView = new System.Windows.Forms.ListView();
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.BallsLabel = new System.Windows.Forms.Label();
+            this.WallsLabel = new System.Windows.Forms.Label();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.FilePreviewBox)).BeginInit();
             this.SuspendLayout();
             // 
             // BackButton
@@ -86,15 +94,6 @@ namespace csci321_assignment02
             this.PathText.TabIndex = 4;
             this.PathText.TextChanged += new System.EventHandler(this.PathText_TextChanged);
             // 
-            // DataBrowser
-            // 
-            this.DataBrowser.Location = new System.Drawing.Point(12, 35);
-            this.DataBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.DataBrowser.Name = "DataBrowser";
-            this.DataBrowser.Size = new System.Drawing.Size(659, 250);
-            this.DataBrowser.TabIndex = 5;
-            this.DataBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.DataBrowser_Navigated);
-            // 
             // ExploreButton
             // 
             this.ExploreButton.Location = new System.Drawing.Point(542, 9);
@@ -105,14 +104,73 @@ namespace csci321_assignment02
             this.ExploreButton.UseVisualStyleBackColor = true;
             this.ExploreButton.Click += new System.EventHandler(this.ExploreButton_Click);
             // 
+            // FilePreviewBox
+            // 
+            this.FilePreviewBox.Location = new System.Drawing.Point(5, 35);
+            this.FilePreviewBox.Name = "FilePreviewBox";
+            this.FilePreviewBox.Size = new System.Drawing.Size(250, 250);
+            this.FilePreviewBox.TabIndex = 7;
+            this.FilePreviewBox.TabStop = false;
+            // 
+            // DataView
+            // 
+            this.DataView.HideSelection = false;
+            this.DataView.Location = new System.Drawing.Point(262, 35);
+            this.DataView.Name = "DataView";
+            this.DataView.Size = new System.Drawing.Size(409, 250);
+            this.DataView.SmallImageList = this.iconList;
+            this.DataView.TabIndex = 8;
+            this.DataView.UseCompatibleStateImageBehavior = false;
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.SizeLabel.Location = new System.Drawing.Point(12, 310);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(39, 13);
+            this.SizeLabel.TabIndex = 9;
+            this.SizeLabel.Text = "Size: 0";
+            // 
+            // BallsLabel
+            // 
+            this.BallsLabel.AutoSize = true;
+            this.BallsLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.BallsLabel.Location = new System.Drawing.Point(71, 310);
+            this.BallsLabel.Name = "BallsLabel";
+            this.BallsLabel.Size = new System.Drawing.Size(41, 13);
+            this.BallsLabel.TabIndex = 10;
+            this.BallsLabel.Text = "Balls: 0";
+            // 
+            // WallsLabel
+            // 
+            this.WallsLabel.AutoSize = true;
+            this.WallsLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.WallsLabel.Location = new System.Drawing.Point(127, 310);
+            this.WallsLabel.Name = "WallsLabel";
+            this.WallsLabel.Size = new System.Drawing.Size(45, 13);
+            this.WallsLabel.TabIndex = 11;
+            this.WallsLabel.Text = "Walls: 0";
+            // 
+            // iconList
+            // 
+            this.iconList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iconList.ImageStream")));
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            this.iconList.Images.SetKeyName(0, "folder.png");
+            this.iconList.Images.SetKeyName(1, "marble.png");
+            // 
             // MarbleExplorer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
-            this.ClientSize = new System.Drawing.Size(680, 307);
+            this.ClientSize = new System.Drawing.Size(680, 332);
+            this.Controls.Add(this.WallsLabel);
+            this.Controls.Add(this.BallsLabel);
+            this.Controls.Add(this.SizeLabel);
+            this.Controls.Add(this.DataView);
+            this.Controls.Add(this.FilePreviewBox);
             this.Controls.Add(this.ExploreButton);
-            this.Controls.Add(this.DataBrowser);
             this.Controls.Add(this.PathText);
             this.Controls.Add(this.PathLabel);
             this.Controls.Add(this.OpenButton);
@@ -123,6 +181,8 @@ namespace csci321_assignment02
             this.Name = "MarbleExplorer";
             this.ShowIcon = false;
             this.Text = "MarbleExplorer";
+            this.Load += new System.EventHandler(this.MarbleExplorer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FilePreviewBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,7 +195,12 @@ namespace csci321_assignment02
         private System.Windows.Forms.Button OpenButton;
         private System.Windows.Forms.Label PathLabel;
         private System.Windows.Forms.TextBox PathText;
-        private System.Windows.Forms.WebBrowser DataBrowser;
         private System.Windows.Forms.Button ExploreButton;
+        private System.Windows.Forms.PictureBox FilePreviewBox;
+        private System.Windows.Forms.ListView DataView;
+        private System.Windows.Forms.Label SizeLabel;
+        private System.Windows.Forms.Label BallsLabel;
+        private System.Windows.Forms.Label WallsLabel;
+        private System.Windows.Forms.ImageList iconList;
     }
 }
--- csci321_assignment02/MarbleExplorer.cs
+++ csci321_assignment02/MarbleExplorer.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +15,27 @@ namespace csci321_assignment02
 {
     public partial class MarbleExplorer : Form
     {
-        public MarbleExplorer()
+        private string filePath { get; set; }
+        private bool isFile = false;
+        private string currentlySelectedItemName = "";
+
+        public MarbleExplorer(string startingPath)
         {
             InitializeComponent();
-            DataBrowser.Url = new Uri(Environment.CurrentDirectory);
+            filePath = PathText.Text = startingPath;
         }
 
         private void PathText_TextChanged(object sender, EventArgs e)
         {
             // This will trigger the browser to refresh with new folder contents
-            DataBrowser.Url = new Uri(PathText.Text);
+            filePath = PathText.Text;
+            LoadFilesAndDirectories();
         }
 
         private void OpenButton_Click(object sender, EventArgs e)
         {
             // This will open the selected file to begin extraction and usage
+            //Console.WriteLine("Line: " + GetSelectedText());
         }
 
         private void ExploreButton_Click(object sender, EventArgs e)
@@ -36,8 +45,7 @@ namespace csci321_assignment02
             {
                 if (fbd.ShowDialog() == DialogResult.OK)
                 {
-                    DataBrowser.Url = new Uri(fbd.SelectedPath);
-                    PathText.Text = fbd.SelectedPath;
+                    filePath = PathText.Text = fbd.SelectedPath;
                 }
             }
         }
@@ -49,25 +57,88 @@ namespace csci321_assignment02
 
         private void BackButton_Click(object sender, EventArgs e)
         {
-            if (DataBrowser.CanGoBack)
-            {
-                DataBrowser.GoBack();
-            }
+            // TODO
         }
 
         private void ForwardButton_Click(object sender, EventArgs e)
         {
-            if (DataBrowser.CanGoForward)
-            {
-                DataBrowser.GoForward();
-            }
+            // TODO
         }
 
-        private void DataBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        private void MarbleExplorer_Load(object sender, EventArgs e)
         {
-            PathText.Text = DataBrowser.Url.LocalPath;
-            BackButton.Enabled = DataBrowser.CanGoBack;
-            ForwardButton.Enabled = DataBrowser.CanGoForward;
+            LoadFilesAndDirectories();
+        }
+
+        public void LoadFilesAndDirectories()
+        {
+            DirectoryInfo fileList;
+            string tempFilePath = "";
+            FileAttributes fileAttr;
+            try
+            {
+
+                if (isFile)
+                {
+                    tempFilePath = filePath + "/" + currentlySelectedItemName;
+                    FileInfo fileDetails = new FileInfo(tempFilePath);
+                    //fileNameLabel.Text = fileDetails.Name;
+                    //fileTypeLabel.Text = fileDetails.Extension;
+                    fileAttr = File.GetAttributes(tempFilePath);
+                    Process.Start(tempFilePath);
+                }
+                else
+                {
+                    fileAttr = File.GetAttributes(filePath);
+
+                }
+
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    fileList = new DirectoryInfo(filePath);
+                    FileInfo[] files = fileList.GetFiles(); // GET ALL THE FILES
+                    DirectoryInfo[] dirs = fileList.GetDirectories(); // GET ALL THE DIRS
+                    string fileExtension = "";
+                    DataView.Items.Clear();
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        fileExtension = files[i].Extension.ToUpper();
+                        switch (fileExtension)
+                        {
+                            /*case ".PNG":
+                            case ".JPG":
+                            case ".JPEG":
+                                DataView.Items.Add(files[i].Name, 9);
+                                break;
+                            case ".TXT":
+                                DataView.Items.Add(files[i].Name, 2);
+                                break;*/
+                            case ".MRB":
+                                DataView.Items.Add(files[i].Name, 1);
+                                break;
+
+                            default:
+                                //DataView.Items.Add(files[i].Name, 8);
+                                break;
+                        }
+
+                    }
+
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        DataView.Items.Add(dirs[i].Name, 0);
+                    }
+                }
+                else
+                {
+                    DataView.Text = this.currentlySelectedItemName;
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO
+            }
         }
     }
 }
